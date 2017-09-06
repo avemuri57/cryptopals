2b8989a6d37eb284c4e1c23ef116391567e9ee71
Anand Vemuri <avemuri57@Anands-MacBook-Pro.local>
2017-09-05 23:37:12 -0400

    updates

--- Set1/1/1.py
+++
@@ -1,9 +0,0 @@
-#Given
-input='49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
-answer='SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
-
-
-#Solution
-
-solution=input.decode('hex').encode('base64')
-print solution 
